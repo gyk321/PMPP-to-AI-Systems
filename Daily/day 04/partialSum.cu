76316091eacd608bf5d9277de3fa0b72aa4822a6
gyk321 <1054060225@qq.com>
2026-08-25 00:59:16 +0800

    Improve comments in partialSumKernel for clarity and understanding

--- Daily/day 04/partialSum.cu
+++ Daily/day 04/partialSum.cu
@@ -1,16 +1,18 @@
 #include<stdio.h>
 
+// 利用 共享内存（Shared Memory） 实现一个块内并行前缀和
 __global__ void partialSumKernel(int *input,int *output,int n){
-  extern __shared__ int sharedMemory[];
+  extern __shared__ int sharedMemory[];   // 声明了一块动态共享内存
 
-  int tid = threadIdx.x;
-  int index = blockIdx.x * blockDim.x * 2 + tid;
+  int tid = threadIdx.x;                            // 当前线程在其所在的线程块（Block）内的局部索引
+  int index = blockIdx.x * blockDim.x * 2 + tid;    // 整个网格（Grid）中的全局数据索引,每个线程块实际上负责处理 2 * blockDim.x 个全局内存元素
 
   if(index < n){
     sharedMemory[tid] = input[index] + input[index + blockDim.x];
     __syncthreads();
 
     // Fixed: start stride at 1 and shift left each iteration.
+    // 它的逻辑是让每个元素去加上它前面的元素，步长不断翻倍（1, 2, 4, 8...）.stride <<= 1 也就是乘 2
     for (int stride = 1; stride < blockDim.x; stride <<= 1){
       int temp = 0;
       if(tid >= stride){
